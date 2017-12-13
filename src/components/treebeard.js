f93fa61e53d2104ad51c07e9f9c6918e156af16b
mapnhka <34423862+mapnhka@users.noreply.github.com>
2017-12-13 10:23:17 +0300

    Update treebeard.js

--- src/components/treebeard.js
+++ src/components/treebeard.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { isEqual } from 'lodash';
 
 import TreeNode from './node';
 import defaultDecorators from './decorators';
@@ -9,8 +10,61 @@ import defaultTheme from '../themes/default';
 import defaultAnimations from '../themes/animations';
 
 class TreeBeard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            draggingNode: null,
+            targetNode: null
+        }
+    }
+
+    handleStart = (node) => {
+        const {handleStart} = this.props;
+        this.setState({
+            draggingNode: node
+        });
+        if (!!handleStart) {
+            handleStart(node);
+        }
+        return false;
+    };
+
+    handleStop = (node) => {
+        const {handleStop} = this.props;
+        const {targetNode, draggingNode} = this.state;
+        this.setState({
+            draggingNode: null,
+            targetNode: null
+        });
+
+        if (!!handleStop && !!draggingNode && !!targetNode) {
+            handleStop(draggingNode, targetNode);
+        }
+    };
+
+    onMouseOver = (node) => {
+        const {draggingNode} = this.state;
+
+        if (!!draggingNode) {
+            this.setState({
+                targetNode: node
+            });
+        }
+    };
+
+    onMouseOut = (node) => {
+        const {draggingNode} = this.state;
+        if (!!draggingNode) {
+            this.setState({
+                targetNode: null
+            });
+        }
+    };
+
     render() {
-        const {animations, decorators, data: propsData, onToggle, style, onContextMenu} = this.props;
+        const {animations, decorators, data: propsData, onToggle, style, onContextMenu, handleDrag, onSelectNode} = this.props;
+        const {draggingNode} = this.state;
         let data = propsData;
 
         // Support Multiple Root Nodes. Its not formally a tree, but its a use-case.
@@ -18,16 +72,26 @@ class TreeBeard extends React.Component {
             data = [data];
         }
         return (
-            <ul style={style.tree.base}
+            <ul className="treebeard" style={style.tree.base}
                 ref={ref => this.treeBaseRef = ref}>
                 {data.map((node, index) =>
                     <TreeNode animations={animations}
                               decorators={decorators}
                               key={node.id || index}
                               node={node}
+                              draggingtree={!!draggingNode}
                               onToggle={onToggle}
+                              onSelectNode={onSelectNode}
+
                               onContextMenu={onContextMenu}
-                              style={style.tree.node}/>
+                              style={style.tree.node}
+
+                              onMouseOver={this.onMouseOver}
+                              onMouseOut={this.onMouseOut}
+                              handleStart={this.handleStart}
+                              handleDrag={handleDrag}
+                              handleStop={this.handleStop}
+                    />
                 )}
             </ul>
         );
@@ -45,6 +109,10 @@ TreeBeard.propTypes = {
         PropTypes.bool
     ]),
     onToggle: PropTypes.func,
+    onSelectNode: PropTypes.func,
+    handleStart: PropTypes.func,
+    handleDrag: PropTypes.func,
+    handleStop: PropTypes.func,
     decorators: PropTypes.object,
 
     onContextMenu: PropTypes.func
